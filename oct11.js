b491fc8d536ed24a2d713dc86f191034f40713a8
niamul007 <am3994566@gmail.com>
2025-10-11 20:20:49 +0600

    adding 2nd practice

--- oct11.js
+++ oct11.js
@@ -1,6 +1,23 @@
+// first practice 
 const arr = [10, 5, 20, 15];
 const reduce = arr.reduce((prev,curr)=> {
     let sum = 0;
     return sum+= prev + curr;
 },0)
 console.log(reduce);
+
+
+
+// 2nd practice 
+
+const niamul = {
+    name: "Niamul",
+    age : 24,
+    weight: "74.5 kg",
+    height: "5.8 ft"
+}
+
+function countProperties(obj){
+    return Object.keys(obj);
+}
+console.log(countProperties(niamul));
