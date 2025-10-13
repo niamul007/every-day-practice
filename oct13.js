c39537018e99088b6c2810f661366df38b587a92
niamul007 <am3994566@gmail.com>
2025-10-13 23:51:11 +0600

    adding last

--- oct13.js
+++ oct13.js
@@ -4,6 +4,20 @@ const names = ['Tom', 'Alice', 'Tom', 'Bob', 'Alice'];
 // newNames.filter((item)=> console.log(item))
 
 
+const seen = new Set();
+const duplicates = new Set();
+names.filter(items=>{
+    if(seen.has(items)){
+        duplicates.add(items)
+    }else{
+        seen.add(items)
+    }
+})
+
+const final = [...duplicates];
+console.log(final);
+
+
 const original = { a: 1, b: { c: 2 } };
 let shalowCopy = {...original};
 shalowCopy.b.c = 5;
