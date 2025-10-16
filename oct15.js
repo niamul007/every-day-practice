e2e6d541d7a485179977c3f63d9e9be3d73c7caf
niamul007 <am3994566@gmail.com>
2025-10-16 11:20:20 +0600

    adding all

--- oct15.js
+++ oct15.js
@@ -8,3 +8,43 @@ const multiply = (a) =>{
 
 console.log(multiply(2)(3)(4));
 
+// const p1 = new Promise((resolve)=>{
+//     setTimeout(()=>{
+//         resolve("one")
+//     },1000);
+// })
+
+// const p2 = new Promise((resolve)=>{
+//     setTimeout(() => {
+//         resolve("two");
+//     }, 2000);
+// });
+
+// Promise.all([p1, p2])
+//   .then((results) => {
+//     console.log(results); // ["One", "Two"]
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
+
+
+const p1 = new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve("One");
+    },1000)
+})
+
+const p2 = new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve("two")
+    },2000)
+})
+
+Promise.all([p1,p2])
+.then((result)=>{
+    console.log(result);
+})
+.catch((error)=>{
+    console.log(error);
+})
