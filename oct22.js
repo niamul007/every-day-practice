07ed94d8ed213d87e2aea6f120edf71691b31e8f
niamul007 <am3994566@gmail.com>
2025-10-23 23:25:59 +0600

    adding promise

--- oct22.js
+++ oct22.js
@@ -1,12 +1,41 @@
 import { it } from "node:test";
 
-
 const inventoryData = [
-    { sku: 'A101', name: 'Premium Coffee', pricing: { unit_price: 15.00, discount: 0 }, stock_level: 105, tags: ['food', 'hot'] },
-    { sku: 'B202', name: 'Espresso Mug', pricing: { unit_price: 8.50, discount: 0.1 }, stock_level: 8, tags: ['kitchen', 'low_stock'] },
-    { sku: 'C303', name: 'Tea Sampler', pricing: { unit_price: 22.00, discount: 0 }, stock_level: 25, tags: ['food'] },
-    { sku: 'D404', name: 'Milk Frother', pricing: { unit_price: 45.00, discount: 0.2 }, stock_level: 15, tags: ['kitchen', 'low_stock'] },
-    { sku: 'E505', name: 'Sugar Packets', pricing: { unit_price: 5.00, discount: 0 }, stock_level: 250, tags: ['food'] }
+  {
+    sku: "A101",
+    name: "Premium Coffee",
+    pricing: { unit_price: 15.0, discount: 0 },
+    stock_level: 105,
+    tags: ["food", "hot"],
+  },
+  {
+    sku: "B202",
+    name: "Espresso Mug",
+    pricing: { unit_price: 8.5, discount: 0.1 },
+    stock_level: 8,
+    tags: ["kitchen", "low_stock"],
+  },
+  {
+    sku: "C303",
+    name: "Tea Sampler",
+    pricing: { unit_price: 22.0, discount: 0 },
+    stock_level: 25,
+    tags: ["food"],
+  },
+  {
+    sku: "D404",
+    name: "Milk Frother",
+    pricing: { unit_price: 45.0, discount: 0.2 },
+    stock_level: 15,
+    tags: ["kitchen", "low_stock"],
+  },
+  {
+    sku: "E505",
+    name: "Sugar Packets",
+    pricing: { unit_price: 5.0, discount: 0 },
+    stock_level: 250,
+    tags: ["food"],
+  },
 ];
 
 // const filterData = inventoryData.filter(item => item.stock_level < 105 );
@@ -20,8 +49,31 @@ const inventoryData = [
 // console.log(filterByTag)
 // console.log(finalPriceSet);
 
+//
 
-// 
+const filterData = inventoryData
+  .filter((item) => !item.tags.includes("low_stock"))
+  .filter((item) => item.stock_level < 150)
+  .map((item) => ({
+    sku: item.sku,
+    name: item.name,
+    finalPrice: (item.pricing.unit_price * (1 - item.pricing.discount)).toFixed(
+      2
+    ),
+  }));
+console.log(filterData);
 
-const filterData = inventoryData.filter( item => !item.tags.includes('low_stock')).filter(item => item.stock_level < 150).map(item=> ({sku: item.sku ,name: item.name , finalPrice: (item.pricing.unit_price * (1 - item.pricing.discount)).toFixed(2)}));
-console.log(filterData)
+
+const p = new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve("Helloe Buddy")
+    },2000)
+})
+const p1 = new Promise((resolve)=>{
+    setTimeout(()=>{
+        resolve("Helloe Buddy")
+    },3000)
+})
+
+const all = Promise.all([p,p1]);
+all.then(result => console.log(result))
