fa2b16d3dfa60211d3fb7b6c29534b89d4bfc3cd
niamul007 <am3994566@gmail.com>
2025-10-24 23:10:20 +0600

    adding filter user and data

--- oct23.js
+++ oct23.js
@@ -1,21 +1,27 @@
 async function aggregateData() {
-    try {
-        const usersURL = "https://jsonplaceholder.typicode.com/users";
-        const postsURL = "https://jsonplaceholder.typicode.com/posts";
+  try {
+    const usersURL = "https://jsonplaceholder.typicode.com/users";
+    const postsURL = "https://jsonplaceholder.typicode.com/posts";
 
-        const [usersResponse, postsResponse] = await Promise.all([
-            fetch(usersURL),
-            fetch(postsURL)
-        ]);
+    const [usersResponse, postsResponse] = await Promise.all([
+      fetch(usersURL),
+      fetch(postsURL),
+    ]);
 
-        const users = await usersResponse.json();
-        const posts = await postsResponse.json();
+    const users = await usersResponse.json();
+    const posts = await postsResponse.json();
 
-        console.log("Users:", users);
-        console.log("Posts:", posts);
-    } catch (error) {
-        console.error("Error loading data:", error);
-    }
+    let filterUser = [...users].map((item) => ({
+      userId: item.id,
+      userName: item.name,
+    }));
+
+    const filterPost = [...posts].filter(item => item.id % 2 ==0).map(item=> item);
+    console.log("Users:", filterUser);
+    console.log("Posts:", filterPost);
+  } catch (error) {
+    console.error("Error loading data:", error);
+  }
 }
 
 aggregateData();
