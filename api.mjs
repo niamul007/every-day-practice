5f9b80e837b28be5a25a598016f5773482b8fbfb
niamul007 <am3994566@gmail.com>
2025-10-29 22:02:09 +0600

    adding initial data

--- api.mjs
+++ api.mjs
@@ -1,13 +1,25 @@
 import dataBase from "./dataBase.mjs";
-import http from 'node:http'
-
+import http from "node:http";
 
 const PORT = 8000;
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 
-
-const server = http.createServer((req,res)=>{
-    res.end("Hello world");
+const server = http.createServer(async (req, res) => {
+  try {
+    if (req.url === "/api" && req.method === "GET") {
+      const destination = await dataBase();
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 200;
+      res.end(JSON.stringify(destination));
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ error: error.message }));
+  }
 });
 
-server.listen(PORT,hostname,()=> console.log(`your url is http://${hostname}:${PORT}`))
+server.listen(PORT, hostname, () =>
+  console.log(`your url is http://${hostname}:${PORT}/api`)
+);
