0872289a8f8f0f9876e0d3f462122f8752724e1f
niamul007 <am3994566@gmail.com>
2025-10-29 22:26:18 +0600

    adding country filter

--- api.mjs
+++ api.mjs
@@ -12,18 +12,27 @@ const server = http.createServer(async (req, res) => {
       res.setHeader("Content-Type", "application/json");
       res.statusCode = 200;
       res.end(JSON.stringify(destination));
-    } 
-    else if (req.url.startsWith("/api/continent") && req.method === "GET") {
-      const continent = req.url.split("/").pop();
-      const filterData = destination.filter(
+    } else if (req.url.startsWith("/api/continent/") && req.method === "GET") {
+      const continentEncoded = req.url.split("/").pop();
+      const continent = decodeURIComponent(continentEncoded);
+      const filterContinent = destination.filter(
         (item) =>
           item.continent.toLocaleLowerCase() === continent.toLocaleLowerCase()
       );
       res.setHeader("Content-Type", "application/json");
       res.statusCode = 200;
-      res.end(JSON.stringify(filterData));
+      res.end(JSON.stringify(filterContinent));
+    } else if (req.url.startsWith("/api/country/") && req.method === "GET") {
+      const countryEncoded = req.url.split("/").pop();
+      const country = decodeURIComponent(countryEncoded);
+      const filterCountry = destination.filter(
+        (item) =>
+          item.country.toLocaleLowerCase() === country.toLocaleLowerCase()
+      );
+      res.setHeader("Content-Type", "application/json");
+      res.statusCode = 200;
+      res.end(JSON.stringify(filterCountry));
     }
-    
   } catch (error) {
     console.error(error.message);
     res.statusCode = 500;
@@ -33,5 +42,7 @@ const server = http.createServer(async (req, res) => {
 });
 
 server.listen(PORT, hostname, () =>
-  console.log(`your url is http://${hostname}:${PORT}/api/continent/africa`)
+  console.log(
+    `your url is http://${hostname}:${PORT}/api/continent/asia  \n for country : http://${hostname}:${PORT}/api/country/japan`
+  )
 );
