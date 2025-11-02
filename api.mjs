aec309f823ac121a3acb83436a2c1af85b63629c
niamul007 <am3994566@gmail.com>
2025-11-02 12:05:22 +0600

    adding some little changes

--- api.mjs
+++ api.mjs
@@ -4,11 +4,14 @@ import sendJSONResponse from "./sendJsonRes.mjs";
 import getDataByPathParams from "./getDataByPathParams.mjs";
 import getQueryParams from "./getQueryParams.mjs";
 
+
+
 const PORT = 8000;
 const hostname = "127.0.0.1";
 
 const server = http.createServer(async (req, res) => {
   try {
+
     const destination = await dataBase();
     const urlObj = new URL(req.url, `http://${req.headers.host}`);
     const queryObj = Object.fromEntries(urlObj.searchParams.entries());
@@ -17,7 +20,8 @@ const server = http.createServer(async (req, res) => {
       const filteredData = getQueryParams(destination, queryObj);
       console.log(queryObj); // Now prints { country: 'norway', is_open_to_public: 'true' }
       sendJSONResponse(res, 200, filteredData);
-    } else if (req.url.startsWith("/api/continent/") && req.method === "GET") {
+    } 
+    else if (req.url.startsWith("/api/continent/") && req.method === "GET") {
       const continentEncoded = req.url.split("/").pop();
       const continent = decodeURIComponent(continentEncoded);
       const filterContinent = getDataByPathParams(
@@ -26,7 +30,8 @@ const server = http.createServer(async (req, res) => {
         continent
       );
       sendJSONResponse(res, 200, filterContinent);
-    } else if (req.url.startsWith("/api/country/") && req.method === "GET") {
+    } 
+    else if (req.url.startsWith("/api/country/") && req.method === "GET") {
       const countryEncoded = req.url.split("/").pop();
       const country = decodeURIComponent(countryEncoded);
       const filterCountry = getDataByPathParams(
@@ -35,14 +40,15 @@ const server = http.createServer(async (req, res) => {
         country
       );
       sendJSONResponse(res, 200, filterCountry);
-    } else {
-      res.setHeader("Content-Type", "application/json");
+    } 
+    else {
       sendJSONResponse(res, 404, {
         error: "not found",
         message: "The requested route does not exist",
       });
     }
-  } catch (error) {
+  } 
+  catch (error) {
     console.error(error.message);
     res.statusCode = 500;
     res.setHeader("Content-Type", "application/json");
