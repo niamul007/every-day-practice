7cedc6321ab94b42150bfc480c3d337147ca1b66
niamul007 <am3994566@gmail.com>
2025-11-02 11:16:42 +0600

    adding practice api

--- practiceApi.mjs
+++ practiceApi.mjs
@@ -1,16 +1,25 @@
 import http from "node:http";
+import path from "node:path";
 
 const PORT = 8000;
 const hostname = "127.0.0.1";
+const __dirname = import.meta.dirname;
+
+function getFilePath(baseDir) {
+  const filePath = path.join(baseDir, "public", "index.html");
+  return filePath;
+}
 
 const server = http.createServer((req, res) => {
   const urlObj = new URL(req.url, `http://${req.headers.host}`);
   const queryObj = Object.fromEntries(urlObj.searchParams.entries());
-
   if (urlObj.pathname === "/api" && req.method === "GET") {
+
+    const absolutePath = getFilePath(__dirname);
+    
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
-    res.end(`Hello there!\nURL object: ${JSON.stringify(queryObj, null, 2)}`);
+    res.end(`Hello there!\nURL object: ${JSON.stringify(queryObj, null, 2)}\nThe file path:${JSON.stringify(absolutePath)}`);
   }
 });
 
