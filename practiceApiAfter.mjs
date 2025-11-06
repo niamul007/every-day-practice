9962f4a09df1269942c950b95553c65af51da1e8
niamul007 <am3994566@gmail.com>
2025-11-06 19:08:37 +0600

    adding practices

--- practiceApiAfter.mjs
+++ practiceApiAfter.mjs
@@ -1,19 +1,32 @@
 import http from "node:http";
 import path from "node:path";
+import * as fs from 'node:fs/promises';
+
 
 const PORT = 8000;
 const hostname = "127.0.0.1";
+const __dirname = import.meta.dirname;
 
-const server = http.createServer((req, res) => {
+function getFilePath(baseDir){
+    const filePath = path.join(baseDir , 'public', 'index.html');
+    return filePath;
+}
+
+const server = http.createServer(async(req, res) => {
 
     const urlObj = new URL(req.url , `http://${req.headers.host}`);
-    const queryObj = Object.fromEntries(urlObj.searchParams.entries())
+    const queryObj = Object.fromEntries(urlObj.searchParams.entries());
+    
+    const absolutePath = getFilePath(__dirname);
+    const content = await fs.readFile(absolutePath);
 
 
   if (urlObj.pathname === "/api" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
+    // res.setHeader("Content-Type", "application/json");
+    res.setHeader("Content-Type", "text/html");
     res.statusCode = 200;
-    res.end(`Hello there!\nURL object: ${JSON.stringify(queryObj, null, 2)}`);
+    // res.end(`Hello there!\nURL object: ${JSON.stringify(queryObj, null, 2)}\nFile Path${absolutePath}`);
+    res.end(content)
 
   }
 
