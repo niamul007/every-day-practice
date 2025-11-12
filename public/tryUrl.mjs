de836fcb08f14915d6bf317185c877579e4baa60
niamul007 <am3994566@gmail.com>
2025-11-12 20:47:12 +0600

    practicing path and urls

--- public/tryUrl.mjs
+++ public/tryUrl.mjs
@@ -1,9 +1,15 @@
 import path from "node:path";
+import { fileURLToPath } from "node:url";
+const filePath = path.join("public", "text/html", "index.html");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PUBLIC_DIR = path.join(__dirname, "public");
 
-const filePath = path.join("public", "images", "ghotbq.png");
+console.log(PUBLIC_DIR);
 
-console.log("Joined path:", filePath);
-console.log("Absolute path:", path.resolve(filePath));
-console.log("Directory name:", path.dirname(filePath));
-console.log("File name:", path.basename(filePath));
-console.log("Extension:", path.extname(filePath));
+
+// console.log("Joined path:", filePath);
+// console.log("Absolute path:", path.resolve(filePath));
+// console.log("Directory name:", path.dirname(filePath));
+// console.log("File name:", path.basename(filePath));
+// console.log("Extension:", path.extname(filePath));
