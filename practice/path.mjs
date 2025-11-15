756822644b3290ccc394ad4e74a692c09ed9f91f
niamul007 <am3994566@gmail.com>
2025-11-15 20:57:52 +0600

    adding url practice a bit

---
+++ practice/path.mjs
@@ -0,0 +1,7 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "public", "index.html");
+console.log(filePath)
