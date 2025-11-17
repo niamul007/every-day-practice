dc2d72383ff30bb20824b90a5f2cc6191c222259
niamul007 <am3994566@gmail.com>
2025-11-17 10:17:26 +0600

    adding practice http

--- practice/pathPractice.mjs
+++ practice/pathPractice.mjs
@@ -2,14 +2,36 @@ import path from "path";
 import { fileURLToPath } from "url";
 import * as fs from "node:fs/promises";
 import http from 'node:http'
-
+import getContent from "../getContentType.mjs";
 const PORT = 8000;
 const hostname = '127.0.0.1';
 
-const server = http.createServer((req,res)=>{
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicDir = path.join(__dirname,'..','public')
+
+
+function filePath(requestedUrl){
+    const fileName = requestedUrl === '/'?'horrorStory.html':requestedUrl;
+    return path.join(publicDir,fileName);
+}
+
+
+const server = http.createServer(async(req,res)=>{
+    try{
+    const urlObj = new URL(req.url, `http://${hostname}`);
+    const requestedPath = urlObj.pathname;
+    const absolutePath = filePath(requestedPath);
+    const ext = path.extname(absolutePath);
+    const contentType = getContent(ext);
+    const content = await fs.readFile(absolutePath);
     res.statusCode = 200;
-    res.setHeader('Content-Type','application.json');
-    res.end("Hello there")
+    res.setHeader('Content-Type',contentType);
+    res.end(content);
+    }
+    catch(error){
+        console.log(error.message)
+    }
 })
 
 
